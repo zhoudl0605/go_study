3f61b993377ebaf1df460c9291867765ddb6a726
Dinglun Zhou <alex.zhou@hyperoptimal.ca>
2020-11-20 20:37:52 -0400

    Update demo

--- 2.Variables/demo/main.go
+++ 2.Variables/demo/main.go
@@ -12,6 +12,8 @@ func main(){
     name = "Alex Zhou"
     age = 23
     isHungry = true
-    
+    // variable in Go lan must be used, or the compiler will give error 
     fmt.Printf("%s is %d years old, if he is hungry: %t\n", name, age, isHungry)
+    fmt.Print("Age: ", age)
+    fmt.Println("Name: ", name)
 }
