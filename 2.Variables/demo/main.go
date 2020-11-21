48ae7668c1e74b02b43822a4d87595ca621426d8
Dinglun Zhou <alex.zhou@hyperoptimal.ca>
2020-11-20 21:03:03 -0400

    finish variable demo

--- 2.Variables/demo/main.go
+++ 2.Variables/demo/main.go
@@ -8,6 +8,10 @@ var(
     isHungry bool
 )
 
+func foo()(int, string){
+	return 10, "abcde"
+}
+
 func main(){
     name = "Alex Zhou"
     age = 23
@@ -16,4 +20,22 @@ func main(){
     fmt.Printf("%s is %d years old, if he is hungry: %t\n", name, age, isHungry)
     fmt.Print("Age: ", age)
     fmt.Println("Name: ", name)
+
+    // Determine and initializing at the same time
+    var s1 string = "Alex Zhou" // not suggest to use this
+    fmt.Println(s1)
+
+    // Type Derivation
+    var s2 = "Alex"
+    fmt.Println(s2)
+
+    // Short variable declaration
+    // only able to use inside of the function
+    s3 := "Acadia University"
+    fmt.Println(s3)
+
+    x,_ := foo()
+    fmt.Println(x)
+    _,y := foo()
+    fmt.Println(y)
 }
